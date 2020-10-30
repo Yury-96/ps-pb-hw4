2f2b1317f956fd74ef2323b893f6416aa43a9547
Yury-96 <fiz12@bk.ru>
2020-10-31 00:46:22 +0300

    'Reliz-3'

--- dz-4.py
+++ dz-4.py
@@ -2,39 +2,53 @@
 import pandas, openpyxl, collections
 #from openpyxl import load_workbook
 
+def popular(listing, pop_name, quantity):
+    pop_name_listing = collections.Counter(listing).most_common(quantity)
+    pop_list=[]
+    for element in pop_name_listing:
+        m=[0]*13
+        m[0]=element[1]
+        for i in range(len(excel_data_dict)):
+            if element[0] in excel_data_dict[i][pop_name]:
+                jear_month_day = str(excel_data_dict[i]['Дата посещения'])
+                m[int(jear_month_day[5:7])] +=1
+                
+        x = [element[0], m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11], m[12]]
+        pop_list.append(x)
+        print(x)
+    return pop_list
+
+
+def excel_write(text, row_begin_records):
+    wb = openpyxl.load_workbook(filename='report.xlsx')
+    sheet = wb['Лист1']
+    for i in range(len(text)):
+        for j in range(14):
+            sheet.cell(row=i+row_begin_records, column=j+1).value = text[i][j]
+    wb.save(filename='report.xlsx')
+    return
+
+
 # Читаем файл ексль и результат передаем в переменную excel_data
 # Переменная excel_data имеет тип <class 'pandas.core.frame.DataFrame'>
 excel_data = pandas.read_excel('logs.xlsx', sheet_name='log2')
 print(excel_data)
 
-browsers = []
 # Преобразуем переменную excel_data в список словарей с помощью метода to_dict()
 # Результат передаем в переменную excel_data_dict
 excel_data_dict = excel_data.to_dict(orient='records')
-
+browsers = []
+items = []
 for element in excel_data_dict:
     browsers.append(element['Браузер'])
+    s = element['Купленные товары'].split(',')
+    for k in s:
+        items.append(k.strip())
+       
+browser_list = popular(browsers, 'Браузер', 7)
+excel_write(browser_list, 5)
 
-popular_browser = collections.Counter(browsers).most_common(7)
-browser_list=[]
-for element in popular_browser:
-    m=[0]*13
-    m[0]=element[1]
-    for i in range(len(excel_data_dict)):
-        if excel_data_dict[i]['Браузер']==element[0]:
-            jear_month_day = str(excel_data_dict[i]['Дата посещения'])
-            m[int(jear_month_day[5:7])] +=1
-            
-    browser = element[0]       
-    x = [browser, m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11], m[12]]
-    browser_list.append(x)
-    #print(x)
-
-wb = openpyxl.load_workbook(filename='report.xlsx')
-sheet = wb['Лист1']
-for i in range(len(browser_list)):
-    for j in range(14):
-        sheet.cell(row=i+5, column=j+1).value = browser_list[i][j]
-wb.save(filename='report.xlsx')
+item_list = popular(items, 'Купленные товары', 7)
+excel_write(item_list, 19)
 
 print('Всё хорошо!')
